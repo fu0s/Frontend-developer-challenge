dee1fc85e8b8fb9b63f35f1ccec1138eacad4f81
El Assal Soufiane <ela.soufiane@gmail.com>
2017-02-12 20:50:45 +0530

    Add files via upload

---
+++ testingController.js
@@ -0,0 +1,49 @@
+define(['app', 'angular', 'gessCmnModule'], function(app, angular, gessCmnModule) {
+  app.controller("TestingController", ['MessageHandler','$scope','$http', 'fileUpload','$httpBackend', function(MessageHandler,$scope,$http, fileUpload,$httpBackend) {
+
+	// Accordion
+	  $scope.myFunction = function (id) {
+	      var x = document.getElementById(id);
+	      if (x.className.indexOf("w3-show") == -1) {
+	          x.className += " w3-show";
+	          x.previousElementSibling.className += " w3-theme-d1";
+	      } else { 
+	          x.className = x.className.replace("w3-show", "");
+	          x.previousElementSibling.className = 
+	          x.previousElementSibling.className.replace(" w3-theme-d1", "");
+	      }
+	  }
+	  $scope.ListDisplay = [] ;
+	  $scope.show = false ;
+	  $scope.showFive = false ;
+	  $scope.showTwo = false ;
+	  //display list video
+	  $scope.display = function () {
+		  $scope.show = true ;
+		 
+		  if($scope.numberPerPage == "25"){
+			  $scope.showTwo = true ;
+		  }else if($scope.numberPerPage == "10"){ 
+			  $scope.showFive = true ;
+			  $scope.showTwo = true ;
+		  }
+		  fillList(10 , 1);
+	  }
+	  $.getJSON("appResources/JSON.json", function(json) {
+		    console.log(json.data[0]); 
+		    $scope.objects = json.data;
+		   
+		});
+
+	function fillList(size , index){
+		for(i = 0  ; i < size ; i++  ){
+			$scope.ListDisplay[i] = $scope.objects[i + (index - 1)*size];
+		}
+	}
+	 
+	
+  }]);
+  
+ 
+  
+});
