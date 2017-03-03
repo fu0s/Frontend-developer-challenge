f181ba41cdd50b852d58c91a9a42b05dd6cf8e99
El Assal Soufiane <ela.soufiane@gmail.com>
2017-03-03 16:43:16 +0530

    Update testingController.js

--- testingController.js
+++ testingController.js
@@ -1,6 +1,101 @@
 define(['app', 'angular', 'gessCmnModule'], function(app, angular, gessCmnModule) {
   app.controller("TestingController", ['MessageHandler','$scope','$http', 'fileUpload','$httpBackend', function(MessageHandler,$scope,$http, fileUpload,$httpBackend) {
 
+	  
+	  $.getJSON("appResources/JSON.json", function(json) {
+		    console.log(json.data); 
+		    $scope.objects = json.data;
+		   
+		});
+	
+	 
+	  //display list video
+	  $scope.display = function () {
+		  fillList($scope.numberPerPage , 1);
+		  getTotalPages($scope.numberPerPage , $scope.objects.length);
+	  }
+	  //go to page
+	  $scope.goTo = function(index ){
+		 
+		  if(index == 0){
+			 
+			  var j = 0 ;
+			  for(i=1 ; i < $scope.pages.length ; i++){
+				  if($scope.pages[i].clicked == true){
+					  $scope.pages[i].clicked = false ;
+					  $scope.pages[i].paginationClass = "w3-hover-black"
+					  j=i-1;
+				  }
+			  }
+			  if(j==0){
+				  j = 1 ;
+			  }
+			  $scope.pages[j]=pageClicked( j , true);
+			  $scope.pages[j].paginationClass = "w3-hover-black w3-red";
+			  fillList($scope.numberPerPage , j);
+		  }else if (index == ($scope.totalPages + 1) ){
+			  
+			  var j = 0 ;
+			  for(i=1 ; i < $scope.pages.length - 1 ; i++){
+				  if($scope.pages[i].clicked == true){
+					  $scope.pages[i].clicked = false ;
+					  $scope.pages[i].paginationClass = "w3-hover-black"
+					  j=i+1;
+				  }
+			  }
+			  if(j==($scope.totalPages + 1)){
+				  j = $scope.totalPages  ;
+			  }
+			  $scope.pages[j]=pageClicked( j , true);
+			  $scope.pages[j].paginationClass = "w3-hover-black w3-red";
+			  fillList($scope.numberPerPage , j);
+		  }else {
+			  
+			  for(i=1 ; i < $scope.pages.length ; i++){
+				  if($scope.pages[i].clicked == true){
+					  $scope.pages[i].clicked = false ;
+					  $scope.pages[i].paginationClass = "w3-hover-black"
+				  }
+			  }
+			  $scope.pages[index]=pageClicked( index , true);
+			  $scope.pages[index].paginationClass = "w3-hover-black w3-red";
+			  fillList($scope.numberPerPage , index);
+		  }
+	  }
+	function fillList(size , index){
+		 $scope.ListDisplay = [] ;
+		if($scope.objects.length > 1){
+			for(i = 0  ; i < size ; i++  ){
+				$scope.ListDisplay[i] = $scope.objects[i + (index - 1)*size];
+			}
+		}
+		else{
+			alert("Object is NULL");
+		}
+		
+	}
+	
+	function getTotalPages(numberPerPage , size){
+		$scope.pages = [] ;
+		$scope.totalPages = size / numberPerPage ;
+		$scope.pages[0]=pageClicked( ">>" , false);
+		for(i = 0  ; i < $scope.totalPages ; i++  ){
+			$scope.pages[i+1]=pageClicked( i+1 , false);
+		}
+		$scope.pages[$scope.totalPages + 1]=pageClicked( "<<" , false);
+	}
+	
+	function pageClicked ( number, clicked){
+		var page = {
+			    number: number,
+			    clicked: clicked,
+				paginationClass: "w3-hover-black",
+			  };
+		
+		return page ;
+		
+	}
+	 
 	// Accordion
 	  $scope.myFunction = function (id) {
 	      var x = document.getElementById(id);
@@ -13,39 +108,33 @@ define(['app', 'angular', 'gessCmnModule'], function(app, angular, gessCmnModule
 	          x.previousElementSibling.className.replace(" w3-theme-d1", "");
 	      }
 	  }
-	  $scope.ListDisplay = [] ;
-	  $scope.show = false ;
-	  $scope.showFive = false ;
-	  $scope.showTwo = false ;
-	  //display list video
-	  $scope.display = function () {
-		  $scope.show = true ;
-		 
-		  if($scope.numberPerPage == "25"){
-			  $scope.showTwo = true ;
-			   fillList(25 , 1);
-		  }else if($scope.numberPerPage == "10"){ 
-			  $scope.showFive = true ;
-			  $scope.showTwo = true ;
-			   fillList(10 , 1);
-		  }
-		 
-	  }
-	  $.getJSON("appResources/JSON.json", function(json) {
-		    console.log(json.data[0]); 
-		    $scope.objects = json.data;
-		   
-		});
-
-	function fillList(size , index){
-		for(i = 0  ; i < size ; i++  ){
-			$scope.ListDisplay[i] = $scope.objects[i + (index - 1)*size];
-		}
-	}
-	 
 	
   }]);
   
- 
+  app.directive('pagesDirective', function($compile) {
+	    return {
+	    	scope: {
+	            key: '=',
+	            
+	        },
+	     template: '<div  ng-repeat="page in key track by $index"><br>'
+		        	+'<li><a ng-click="goTo($index)" ng-class="page.paginationClass" >{{page.number}}</a></li>'
+		        	+'</div>    ',
+	      replace: true,
+	      link: function($scope, element) {
+	    	  $scope.$watch('pages', function() {
+		    	 
+		        var el = angular.element('<span/>');
+		       
+		        $compile(el)($scope);
+		        element.empty();
+		        element.append(el);
+		     
+		        },true)
+		       
+	           
+	      }
+	    }
+	  });
   
 });
